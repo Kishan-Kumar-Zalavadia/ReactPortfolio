799a137119ab65d19de1451db9bfcdb2cbab774e
Kishan-Kumar-Zalavadia <kishankumar3131@gmail.com>
2022-05-16 11:16:48 +0530

    init

---
+++ src/component/Portfolio/Portfolio_data.js
@@ -0,0 +1,52 @@
+const Portfolio_data = [
+  {
+    id: 1,
+    category: "Portfolio",
+    totalLike: "600",
+    title: "My Portfolio Website-1",
+    image: "./image/portfolio1.png",
+    link: "https://kishankumarz.netlify.app/",
+    para:"sfasasdadfa"
+  },
+  {
+    id: 2,
+    category: "Portfolio",
+    totalLike: "750",
+    title: "My Portfolio Website-2",
+    image: "./image/portfolio2.png",
+    link: "https://kishankumar.netlify.app/",
+  },
+  {
+    id: 3,
+    category: "Image Finding",
+    totalLike: "630",
+    title: "Search any image in this site",
+    image: "./image/findthepicture.png",
+    link: "https://findtheimage.netlify.app/",
+  },
+  {
+    id: 4,
+    category: "Gaming",
+    totalLike: "360",
+    title: "Gaming Website Layout",
+    image: "./image/gaming.png",
+    link: "https://kkgaming.netlify.app/",
+  },
+  {
+    id: 5,
+    category: "My Project Expo",
+    totalLike: "280",
+    title: "Some of my web porjects display",
+    image: "./image/projects.png",
+    link: "https://myprojectexpo.netlify.app/",
+  },
+  {
+    id: 6,
+    category: "Sticky Notes",
+    totalLike: "690",
+    title: "App for taking Sticky Notes",
+    image: "./image/stickynotes.png",
+    link: "https://stickthenote.netlify.app/",
+  },
+]
+export default Portfolio_data
