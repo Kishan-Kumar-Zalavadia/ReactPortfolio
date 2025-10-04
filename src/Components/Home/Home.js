889094ba5b66dc12134ea5b5fca55c06b0e8be9b
Kishan Kumar Zalavadia <kishankumarz1048@gmail.com>
2025-10-04 14:57:53 -0500

    Home Section - Inroduction updated and the image glass ends at same level

--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -11,13 +11,30 @@ const Home = () => {
       <div className="home-container">
         <GlassCard className="home-intro">
           <h3>Hi, I’m Kishan Kumar</h3>
-          <h4>a Web Developer.</h4>
+          <h4>a Software Developer.</h4>
           <p>
-            I am an enthusiastic engineering fresher who is capable of using
-            technical skills for the betterment of the organization. A
-            bachelor’s degree in Software Engineering has enhanced my software
-            skills and looking forward to work in a competitive environment that
-            enhances overall learning.
+            I’m a results-driven Software Engineer passionate about building
+            high-performance, user-centric web applications. With expertise in
+            full-stack development using Java, Spring Boot, ASP.NET, Angular,
+            ReactJS, and MySQL, I thrive on solving complex problems and
+            delivering scalable, efficient solutions.
+          </p>
+
+          <p>
+            I have hands-on experience working on diverse projects, from machine
+            learning research and predictive modeling to developing interactive
+            web applications and SaaS platforms. Whether contributing to
+            cutting-edge research at the University of North Texas or optimizing
+            enterprise applications during my internships, I focus on
+            innovation, precision, and collaboration.
+          </p>
+
+          <p>
+            Driven by curiosity and a commitment to continuous learning, I enjoy
+            pushing the boundaries of my skills and leveraging technology to
+            create impactful solutions. Explore my portfolio to see my projects,
+            experiences, and contributions in software engineering, full-stack
+            development, and machine learning.
           </p>
         </GlassCard>
 
