0ba5301d66507d9fdb6022b4607e401607b8525d
Kishan Kumar Zalavadia <kishankumarz1048@gmail.com>
2025-10-04 12:40:10 -0500

    Navbar Section - Added Hamburger for mobile view

--- src/Components/NavBar/NavBar.js
+++ src/Components/NavBar/NavBar.js
@@ -1,15 +1,21 @@
-import React from "react";
+import React, { useState } from "react";
 import GlassCard from "../../Elements/GlassCard/GlassCard";
 import "./NavBar.css";
 import profileLogo from "../../component-old/pic/logo.jpeg";
+import { FaBars, FaTimes } from "react-icons/fa";
 
 const NavBar = () => {
+  const [menuOpen, setMenuOpen] = useState(false);
+
+  const toggleMenu = () => setMenuOpen(!menuOpen);
+
   return (
     <GlassCard className="navbar">
       <div className="navbar-left">
         <img src={profileLogo} alt="Profile" className="navbar-logo" />
       </div>
 
+      {/* Desktop menu */}
       <div className="navbar-right">
         <a href="#home">Home</a>
         <a href="#skills">Skills</a>
@@ -19,6 +25,38 @@ const NavBar = () => {
         <a href="#certifications">Certifications</a>
         <a href="#contact">Contact Me</a>
       </div>
+
+      {/* Hamburger menu button (visible on mobile) */}
+      <div className="hamburger" onClick={toggleMenu}>
+        {menuOpen ? <FaTimes /> : <FaBars />}
+      </div>
+
+      {/* Mobile dropdown menu */}
+      {menuOpen && (
+        <div className="mobile-menu">
+          <a href="#home" onClick={toggleMenu}>
+            Home
+          </a>
+          <a href="#skills" onClick={toggleMenu}>
+            Skills
+          </a>
+          <a href="#education" onClick={toggleMenu}>
+            Education
+          </a>
+          <a href="#experience" onClick={toggleMenu}>
+            Experience
+          </a>
+          <a href="#projects" onClick={toggleMenu}>
+            Projects
+          </a>
+          <a href="#certifications" onClick={toggleMenu}>
+            Certifications
+          </a>
+          <a href="#contact" onClick={toggleMenu}>
+            Contact Me
+          </a>
+        </div>
+      )}
     </GlassCard>
   );
 };
