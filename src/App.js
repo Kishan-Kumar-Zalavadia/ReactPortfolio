7e638ed78593a71406180a4c317d8d29364c5e61
Kishan Kumar Zalavadia <kishankumarz1048@gmail.com>
2025-10-04 15:27:14 -0500

    Scool top - button added

--- src/App.js
+++ src/App.js
@@ -1,13 +1,4 @@
-import React from "react";
-// import Header from "./component/Head/Header";
-// import Features from "./component/Features/Features";
-// import Home from "./component/Hero/Home";
-// import Portfolio from "./component/Portfolio/Portfolio";
-// import Resume from "./component/Resume/Resume";
-// import Testimonial from "./component/Testimonial/Testimonial";
-// import Blog from "./component/Blog/Blog";
-// import Contact from "./component/Contact/Contact";
-// import Footer from "./component/Footer/Footer";
+import React, { useState, useEffect } from "react";
 import "./App.css";
 import NavBar from "./Components/NavBar/NavBar";
 import Home from "./Components/Home/Home";
@@ -18,18 +9,43 @@ import Projects from "./Components/Projects/Projects";
 import Contact from "./Components/Contact/Contact";
 
 const App = () => {
+  const [showScroll, setShowScroll] = useState(false);
+
+  // Show button when scrolling down
+  useEffect(() => {
+    const handleScroll = () => {
+      if (window.scrollY > 300) setShowScroll(true);
+      else setShowScroll(false);
+    };
+
+    window.addEventListener("scroll", handleScroll);
+    return () => window.removeEventListener("scroll", handleScroll);
+  }, []);
+
+  // Scroll to top function
+  const scrollToTop = () => {
+    window.scrollTo({
+      top: 0,
+      behavior: "smooth",
+    });
+  };
+
   return (
-    <>
-      <div className="main-container">
-        <NavBar />
-        <Home />
-        <Skills />
-        <Education />
-        <Experiences />
-        <Projects />
-        <Contact />
-      </div>
-    </>
+    <div className="main-container">
+      <NavBar />
+      <Home />
+      <Skills />
+      <Education />
+      <Experiences />
+      <Projects />
+      <Contact />
+
+      {showScroll && (
+        <div className="scroll-top" onClick={scrollToTop}>
+          <div className="up-arrow"></div>
+        </div>
+      )}
+    </div>
   );
 };
 
